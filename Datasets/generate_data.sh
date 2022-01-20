3a6995708d8616c7d32e74c5fbde6486c602bd7e
Nelson Filipe Costa <nelsoncosta.jr17@gmail.com>
2022-01-20 17:19:12 -0500

    Updated generate_data.sh.

--- Datasets/generate_data.sh
+++ Datasets/generate_data.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Verifying folder 'era5'..."
-(( -d era5 )) || mkdir era5
+[[ -d era5 ]] || mkdir era5
 
 echo "Verifying folder 'global_fishing_watch'..."
-(( -d global_fishing_watch )) || mkdir global_fishing_watch
+[[ -d global_fishing_watch ]] || mkdir global_fishing_watch
 
 echo "Verifying folder 'data'..."
-(( -d data )) || mkdir data
+[[ -d data ]] || mkdir data
 
 echo "Phase 1 -  Download ERA5 Dataset"
 python download_era5.py
