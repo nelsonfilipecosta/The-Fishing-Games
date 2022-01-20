9ce9247e3a690b7832af493c601e5337e15405ca
Nelson Filipe Costa <nelsoncosta.jr17@gmail.com>
2022-01-20 16:42:14 -0500

    Updated shell script.

--- Datasets/generate_data.sh
+++ Datasets/generate_data.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
+echo "Verifying folder 'era5'..."
+(( -d era5 )) || mkdir era5
+
+echo "Verifying folder 'global_fishing_watch'..."
+(( -d global_fishing_watch )) || mkdir global_fishing_watch
+
+echo "Verifying folder 'data'..."
+(( -d data )) || mkdir data
+
+echo "Phase 1 -  Download ERA5 Dataset"
 python download_era5.py
+
+echo "Phase 2 -  Convert ERA5 Dataset to CSV"
 python convert_era5.py
+
+echo "Phase 3 -  Cleaning Global Fishing Watch Dataset"
 python clean_gfw.py
-python augment_gfw_era5.py
+
+echo "Phase 4 -  Augmenting Global Fishing Watch Dataset with ERA5 Dataset"
+python augment_gfw.py
